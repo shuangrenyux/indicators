9f4f12d3cdf5166d948ae39641e9b2820dad4eb1
Byron <wizard0530@gmail.com>
2013-05-21 09:29:56 +0800

    Clean useless code
    Clean useless code

---
+++ indicators.py
@@ -0,0 +1,23 @@
+def getEMAList(inlist,num):
+    k = 2.0/(num+1.0)
+    ema = inlist[0]
+    emaList = [ema]
+    for item in inlist:
+        ema = item*k+ema*(1-k)
+        emaList.append(ema)
+    return emaList
+
+
+def getMACDList(inList,short,long,mid):
+    shortEMAList = getEMAList(inList,short)
+    longEMAList = getEMAList(inList,long)
+    macdList =[]
+    diffList =[]
+    for i in range(0,len(inList)+1):
+        diff= shortEMAList[i]-longEMAList[i]
+        diffList.append(diff)
+        dea = getEMAList(diffList,mid)[-1]
+        macd = [diffList[i],dea,(diffList[i]-dea)*2]
+        macdList.append(macd)
+    return macdList;
+
